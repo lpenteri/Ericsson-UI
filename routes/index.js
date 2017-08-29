49641660b57c8d59432f6fe8ac5e29bc39f49ca2
lpenteri <lp@ortelio.co.uk>
2017-08-29 16:15:13 +0100

    skype integration

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+const path = require('path');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'MARIO presents 5G, powered by Ericsson' });
+//    res.sendFile(path.join(__dirname + '/../views/index.html'));
+});
+
+module.exports = router;
